5af5d40352038f642ed2a5de2629b63c557e4456
richardtmiles <richard@miles.systems>
2024-03-07 19:07:02 -0700

    wordpress carbon composer updater

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'CarbonPHP' );
+define( 'DB_NAME', 'CarbonWordPress' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
